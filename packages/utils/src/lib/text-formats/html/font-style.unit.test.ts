242020178bb3c12fbe5d44ac373415caefccf12b
Michael <michael.hladky@push-based.io>
2024-05-20 18:16:35 +0200

    refactor(utils): polish tests

--- packages/utils/src/lib/text-formats/html/font-style.unit.test.ts
+++ packages/utils/src/lib/text-formats/html/font-style.unit.test.ts
@@ -2,21 +2,18 @@ import { bold, code, italic } from './font-style';
 
 describe('bold', () => {
   it('should return bold text', () => {
-    const result = bold('Hello World');
-    expect(result).toBe('<b>Hello World</b>');
+    expect(bold('Hello World')).toBe('<b>Hello World</b>');
   });
 });
 
 describe('italic', () => {
   it('should return italic text', () => {
-    const result = italic('Hello World');
-    expect(result).toBe('<i>Hello World</i>');
+    expect(italic('Hello World')).toBe('<i>Hello World</i>');
   });
 });
 
 describe('code', () => {
   it('should return code text', () => {
-    const result = code('Hello World');
-    expect(result).toBe('<code>Hello World</code>');
+    expect(code('Hello World')).toBe('<code>Hello World</code>');
   });
 });
